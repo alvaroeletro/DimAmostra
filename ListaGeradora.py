41ecce2c27726e118e724145ef58ac23815d53ff
alvaroeletro <alvaro_eletro@hotmail.com>
2020-06-16 14:58:24 -0300

    Versão estável

--- ListaGeradora.py
+++ ListaGeradora.py
@@ -3,25 +3,25 @@ print("Bem vindo\n")
 print("Calculo do Tamanho de uma amostra necessária\n")
 print("V.0.01 versão inicial\n")
 print(".....::: Admita as seguintes condições :::.....\n")
-print("Utilização de valores da curva T-student\n")
-print("Nível de significância alpha 5%")
-print("o ível de confiabilidade é de 95%")
-print("n0-1 para grau de liberdade | 9 |\n")
+
 
 contador = 0
+erro=float(input("Inserir erro pré-fixado "))
 x=0
+s=0
 espacoamostral = []
 somax=0
-n0=10
-gl = n0-1
+somaXi=0
+n0=int(input("Insira a qntd. da Amostra Piloto "))
+gl = (n0-1)
+t=input("Verifique na tabela o valor de talpha/2 ")
 
-#n0 SERÁ FIXADO EM 10 PELO FATO DE QUE SE ESSE VALOR FOR ALTERADO
+
+
 #NECESSITA SER CONSULTADO EM TABELA
-#ESSA FUNCIONALIDADE NÃO ESTÁ DISPONÍVEL NESSA VERSÃO DO PROGRAMA
-#gl = grau de liberdade
+print("Certifique-se de que os valores inseridos correponder a curva T-student")
 
-
-while contador<10:
+while contador<n0:
     contador+=1
     x=(int(input("X{}".format(contador))))
     espacoamostral.append(x)
@@ -34,7 +34,7 @@ print("\n Início do calculo\n ")
 contador = 0
 
 #CALCULO DO SOMATÓRIO DE X
-while contador<10:
+while contador<n0:
     x=0
     x=espacoamostral[(contador)]
     somax=somax+x
@@ -42,6 +42,7 @@ while contador<10:
 
 #CALCULO DA MEDIA
 media = somax/n0
+print(" A média calculada é de {}".format(media))
 
 #CALCULO DO DESVIO PADRÃO
 
@@ -49,30 +50,27 @@ Xi=0
 contador = 0
 somaparcelas = 0
 #somatório da parcela
-while contador<10:
+while contador<n0:
     Xi=espacoamostral[(contador)]
-    parcela=((Xi-somax)**2)
+    Xi= (Xi-media)**2
+    somaXi += Xi
     contador+=1
-    somaparcelas += parcela
 
+
+#print("Soma das parcelas {}".format(somaparcelas))
 #calculo do desvio padrao com o Somatório pronto
-print(somaparcelas)
 
-s= somaparcelas/gl
-s = s ** 0.5
-
+s= (somaXi/(gl))
+s= (s**(0.5))
 
 
-
-print(" A média calculada é de {}".format(media))
 print("Desvio padrão calculado {}".format(s))
-
 
 #FINALMENTE
 #CALCULO DA AMOSTRA MÍNIMA
 
-amostramin= (2.26*(s))
-amostramin=amostramin/3
+amostramin=float(t)*float(s)
+amostramin=amostramin/erro
 amostramin=amostramin**2
 
 print("A amostra minima para esse experimento é de {}".format(amostramin))
